b43244da7288bd32ddfb99f03004994fd7569560
Malte Jensen <malt656c@stud.kea.dk>
2023-09-23 18:26:20 +0200

    first

---
+++ script.js
@@ -0,0 +1,15 @@
+/* scroll animationer */
+const scrollObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+
+hiddenElements.forEach((e) => scrollObserver.observe(e));
+/* scroll animationer */
